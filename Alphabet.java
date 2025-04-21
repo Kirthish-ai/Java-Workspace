a2d1df0e389f012e93b83a92a7fa54293147541f
KRIZZY.06 <2024.kirthishs@isu.ac.in>
2025-04-21 11:24:19 +0530

    Add files via upload

---
+++ Alphabet.java
@@ -0,0 +1,12 @@
+public class Alphabet 
+{
+    public static void main(String[] args) 
+    {
+        for(char ch = 'a'; ch<='z' ; ch++)
+        {
+            System.out.println(ch+" ");
+        }
+        
+    }
+} 
+
