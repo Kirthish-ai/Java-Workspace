262b4963f936ddbccb2359392d65f5541a0492a6
KRIZZY.06 <2024.kirthishs@isu.ac.in>
2025-04-22 10:00:51 +0530

    Add files via upload

---
+++ Decrement.java
@@ -0,0 +1,14 @@
+class Decrement 
+{
+    public static void main(String[] args) 
+    {
+        int number = 10;
+    
+        System.out.println("Initial value of number: " + number);
+        System.out.println("Post-decrement (number--): " + number--);
+        System.out.println("Value of number after post-decrement: " + number);
+        System.out.println("Pre-decrement (--number): " + --number);
+        System.out.println("Value of number after pre-decrement: " + number);
+    }
+    
+}
